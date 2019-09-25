513314b3257daa83d99db852af382befee2cfdb5
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-09-25 15:37:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dds-client
+
+A golang client for the dds GraphQL API
---
+++ main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/akamensky/argparse"
+	"github.com/machinebox/graphql"
+)
+
+type AppsResponse struct {
+	AppsWrapper AppsWrapper `json:"apps"`
+}
+
+type AppsWrapper struct {
+	Apps     []App `json:"apps"`
+	NextPage int   `json:"nextPage"`
+}
+
+type App struct {
+	Name string `json:"name"`
+}
+
+type AddAppResponse struct {
+	AddApp AddApp `json:"addApp"`
+}
+
+type AddApp struct {
+	App   App    `json:"app"`
+	Error string `json:"error"`
+}
+
+var ClientURL = os.Getenv("DASH_API_URL")
+var Username = os.Getenv("DASH_API_USER")
+var ApiKey = os.Getenv("DASH_API_KEY")
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+func appsList() {
+	req := graphql.NewRequest(`
+query apps($allApps: Boolean) {
+  apps(allApps: $allApps) {
+    nextPage
+    apps {
+      name
+    }
+  }
+}
+`)
+
+	req.Var("allApps", true)
+
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Authorization", "Basic "+basicAuth(Username, ApiKey))
+
+	ctx := context.Background()
+
+	var respData AppsResponse
+
+	client := graphqlClient()
+	if err := client.Run(ctx, req, &respData); err != nil {
+		log.Fatal(err)
+	}
+	for _, app := range respData.AppsWrapper.Apps {
+		fmt.Printf("%v\n", app.Name)
+	}
+}
+
+func appsCreate(name string) {
+	if name == "" {
+		log.Fatal(errors.New("No name specified"))
+	}
+
+	req := graphql.NewRequest(`
+mutation AddApp($name: String!) {
+  addApp(name: $name) {
+    app {
+      name
+    }
+    error
+  }
+}
+`)
+
+	req.Var("name", name)
+
+	req.Header.Set("Cache-Control", "no-cache")
+	req.Header.Set("Authorization", "Basic "+basicAuth(Username, ApiKey))
+
+	ctx := context.Background()
+
+	var respData AddAppResponse
+
+	client := graphqlClient()
+	if err := client.Run(ctx, req, &respData); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("====> %v created!\n", respData.AddApp.App.Name)
+}
+
+func graphqlClient() *graphql.Client {
+	httpclient := &http.Client{}
+	if true {
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		httpclient = &http.Client{Transport: tr}
+	}
+
+	return graphql.NewClient(ClientURL, graphql.WithHTTPClient(httpclient))
+}
+
+func main() {
+	parser := argparse.NewParser("dds-client", "A simple dds client")
+	name := parser.String("", "name", &argparse.Options{Help: "Name of app"})
+
+	appsListCmd := parser.NewCommand("apps:list", "List all apps")
+	appsCreateCmd := parser.NewCommand("apps:create", "Create an app")
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+		return
+	}
+
+	if appsListCmd.Happened() {
+		appsList()
+	} else if appsCreateCmd.Happened() {
+		appsCreate(*name)
+	} else {
+		err := fmt.Errorf("bad arguments, please check usage")
+		fmt.Print(parser.Usage(err))
+	}
+}
